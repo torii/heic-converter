19e76d39a37b1a508f0d086c0a50b8b950eb1732
Yoshimitsu Torii <yoshimitsu.torii@gmail.com>
2026-03-18 13:14:58 +0900

    fix error

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -99,6 +99,8 @@ fn convert_heic(input_path: String, output_dir: String, quality: u8) -> ConvertR
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![convert_heic])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
